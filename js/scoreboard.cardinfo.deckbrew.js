644f87e2b83217031fe706be32b670d3d53191a8
R3d5tar <github@rinzedijkstra.nl>
2015-12-18 22:54:06 +0100

    updated all license references, updated header to show visual difference

--- js/scoreboard.cardinfo.deckbrew.js
+++ js/scoreboard.cardinfo.deckbrew.js
@@ -1,6 +1,10 @@
 /*
-* Copyright (c) 2014-2015 Filipe Badaro
-* License: https://github.com/Badaro/EDHScoreboard/blob/master/LICENSE
+* 	Copyright (c) 2015 R3d5tar
+*	License: https://github.com/R3d5tar/EDHScoreboard/blob/master/LICENSE
+*	
+*	Based on:
+*		Copyright (c) 2014-2015 Filipe Badaro
+*		License: https://github.com/Badaro/EDHScoreboard/blob/master/LICENSE
 */
 
 scoreboard.cardinfo = {
--- js/scoreboard.settings.js
+++ js/scoreboard.settings.js
@@ -1,1 +1,26 @@
-/** Copyright (c) 2014-2015 Filipe Badaro* License: https://github.com/Badaro/EDHScoreboard/blob/master/LICENSE*/scoreboard.settings = {	startingLife: 40,	minLife: 0,	maxLife: null,	minLifeDisplay: 0,	maxLifeDisplay: 40,	startingPoison: 0,	minPoison: 0,	maxPoison: 10,	minPoisonDisplay: 0,	maxPoisonDisplay: 10,	startingCommanderDamage: 0,	minCommanderDamage: 0,	maxCommanderDamage: 21,	minCommanderDamageDisplay: 0,	maxCommanderDamageDisplay: 21}
+/*
+* 	Copyright (c) 2015 R3d5tar
+*	License: https://github.com/R3d5tar/EDHScoreboard/blob/master/LICENSE
+*	
+*	Based on:
+*		Copyright (c) 2014-2015 Filipe Badaro
+*		License: https://github.com/Badaro/EDHScoreboard/blob/master/LICENSE
+*/
+
+scoreboard.settings = {
+	startingLife: 40,
+	minLife: 0,
+	maxLife: null,
+	minLifeDisplay: 0,
+	maxLifeDisplay: 40,
+	startingPoison: 0,
+	minPoison: 0,
+	maxPoison: 10,
+	minPoisonDisplay: 0,
+	maxPoisonDisplay: 10,
+	startingCommanderDamage: 0,
+	minCommanderDamage: 0,
+	maxCommanderDamage: 21,
+	minCommanderDamageDisplay: 0,
+	maxCommanderDamageDisplay: 21
+}
