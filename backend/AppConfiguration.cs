d00c28982f499d27ecefb8bdb3e068efe14905f7
R3d5tar <github@rinzedijkstra.nl>
2018-06-29 21:21:09 +0200

    added logging about cors configuration in backend

--- backend/AppConfiguration.cs
+++ backend/AppConfiguration.cs
@@ -7,13 +7,13 @@ namespace EDHScoreboard.Backend
 	{
 		public string[] AllowedCorsOrigins { get; set; }
 
-		public string [] BuildAllowedCorsOrigins() {
+		public string[] BuildAllowedCorsOrigins()
+		{
 			string[] EMPTY_ARRAY = new string[0];
 			var envVar = Environment.GetEnvironmentVariable("ALLOWED_CORS_ORIGINS");
-			var origins = envVar?.Split(',',StringSplitOptions.RemoveEmptyEntries) ?? EMPTY_ARRAY;
-			
-			return origins.AsEnumerable().Concat(AllowedCorsOrigins ?? EMPTY_ARRAY).ToArray();
+			var origins = envVar?.Split(',', StringSplitOptions.RemoveEmptyEntries) ?? EMPTY_ARRAY;
 
+			return origins.AsEnumerable().Concat(AllowedCorsOrigins ?? EMPTY_ARRAY).ToArray();
 		}
 	}
 }
--- backend/Startup.cs
+++ backend/Startup.cs
@@ -6,62 +6,68 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace EDHScoreboard.Backend
 {
 	public class Startup
-    {
+	{
 		private const string CorsPolicyName = "CorsPolicy";
+		private ILogger<Startup> _logger;
 		public IConfiguration Configuration { get; }
 		public AppConfiguration App { get; } = new AppConfiguration();
 
 		public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
+		{
+			Configuration = configuration;
 			Configuration.GetSection("App").Bind(App);
-        }
+		}
 
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
 			//add Cors Policy
+			var corsOrigins = App.BuildAllowedCorsOrigins();
 			services.AddCors(options => options.AddPolicy(CorsPolicyName,
 				builder => builder
 					.AllowAnyMethod()
 					.AllowAnyHeader()
 					.AllowCredentials()
-					.WithOrigins(App.BuildAllowedCorsOrigins()))
+					.WithOrigins(corsOrigins))
 			);
 
 			// default uri: /swagger
-			services.AddSwaggerGen(c => c.SwaggerDoc("v1", 
+			services.AddSwaggerGen(c => c.SwaggerDoc("v1",
 				new Info { Title = "EDHScoreboard - Backend API", Version = "v1" })
 			);
 
 			services.AddSignalR();
 
 			services.AddSingleton<Dictionary<string, GameAdministration>>();
-        }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
+			var provider = services.BuildServiceProvider();
+			_logger = provider.GetService<ILogger<Startup>>();
+			LogCorsConfig(corsOrigins);
+		}
 
-			//app.UseHttpsRedirection();
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseHsts();
+			}
+
 			app.UseCors(CorsPolicyName);
 			app.UseSignalR(routes =>
-            {
-                routes.MapHub<Hubs.MsgHub>("/msgHub");
-            });
+			{
+				routes.MapHub<Hubs.MsgHub>("/msgHub");
+			});
 
 			app.UseSwagger();
 			app.UseSwaggerUI(c =>
@@ -69,7 +75,13 @@ namespace EDHScoreboard.Backend
 				c.SwaggerEndpoint("/swagger/v1/swagger.json", "EDHScoreboard - Backend API / v1");
 			});
 
-            app.UseMvc();			
-        }
-    }
+			app.UseMvc();
+		}
+
+		private void LogCorsConfig(string[] allowedCorsOrigins)
+		{
+			var values = string.Join(Environment.NewLine, allowedCorsOrigins);
+			_logger.LogInformation($"Allowed Cors Origins: {Environment.NewLine}{values}");
+		}
+	}
 }
